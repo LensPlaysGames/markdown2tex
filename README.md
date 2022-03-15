f34d9ddb848380b6caffe7ca31a842ed80bb4b16
LensPlaysGames <lensplaysgames@gmail.com>
2022-03-14 22:14:18 -0700

    Initial Commit
    And everything is within it :^)

---
+++ README.md
@@ -0,0 +1,50 @@
+This python script converts a markdown file given on the command line into a TeX file utilizing the GNU Texinfo set of macros.
+
+NOTE: This is a work in progress; feel free to submit any pull requests of any changes that you have implemented. [Github Repository](https://www.github.com/user/LensPlaysGames/markdown2tex)
+
+# Dependencies
+All of the dependencies for this program are free software (open source).
+
+- [Python](https://www.python.org/)
+- [GNU Texinfo](https://www.gnu.org/software/texinfo/)
+
+# Conversion
+
+## What is a Valid Markdown File to this Parser?
+A `#` at the beginning of a line indicates that a new chapter is started.
+
+Any subsequent `##` will be sections of the most recently decalred chapter.
+
+Any subsequent `###` will be subsections of the most recently declared section. 
+
+Any subsequent `####` will be subsubsections of the most recently declared subsection.
+
+If, for example, a subsubsection is declared without a parent subsection, GNU Texinfo will give a warning about having to "raise" it up, but in the end everything will still work.
+
+If the chapter/section/subsection/subsubsection structure is confusing, look into the `info` pages of `info` itself, or even `texinfo`.
+
+This document is also an example of what this parser can understand, and will be what we will be converting as our example in the next section.
+
+## Converting This File into an Info Page
+We will first convert this README markdown file into a Texinfo source file. 
+
+We can then use Texinfo to convert that source file into an info file that can be viewed with the `info` command.
+
+In a terminal that is open on a machine with the required dependencies installed, invoke the following commands:
+
+`cd /path/to/markdown2tex/`
+
+`python main.py -t "Markdown2Tex Converter" README.md`
+
+`makeinfo markdown2tex_converter.tex`
+
+Everything is all generated at this point! Take a look using `info`:
+
+`info -f markdown2tex_converter.info`
+
+## Command Line Flags and Options
+To get help with the options and flags that may be specified, use the following command:
+
+`python /path/to/markdown2tex/main.py -h`
+
+This will print out the layout of the command (usage), as well as the flags and options available and a short description of what they do to the standard output.
---
+++ main.py
@@ -0,0 +1,265 @@
+from os import path
+from re import findall, search
+from sys import argv
+
+# TODO:
+# `-- Implement '\' end of line (hide in TeX output)
+
+def print_usage():
+    print("Markdown to GNU Texinfo Converter by Rylan `Lens` Kellogg")
+    print()
+    print("Usage:")
+    print("    python main.py [FLAGS] [OPTIONS] \"path/to/markdown.md\"")
+    print()
+    print("Flags:")
+    print("    -h, --help     -- Show the help dialog in stdout")
+    print("    -i, --inline   -- Do not specify new nodes for sections within chapters.")
+    print()
+    print("Options:")
+    print("    -t <title>     -- Specify the title of the Texinfo manual that is generated")
+    print("    -o <path>      -- Specify the filepath where the Texinfo manual will be saved to")
+    print()
+    exit(0)
+
+    
+def parse_headers(src, inline):
+    lines = src.split('\n')
+    for i in range(len(lines)):
+        line = lines[i]
+        if len(line) < 2:
+            continue
+
+        count = 0
+        while (line[count] == '#'):
+            count += 1
+
+        if count == 0:
+            continue
+
+        if count > 4:
+            lines[i] = line[count+1:]
+            continue
+        
+        name = line[count:]
+
+        # Always specify a new node for each chapter,
+        # but do not for any other section if inline is specified.
+        if count == 1 or not inline:
+            line = "@node" + name + '\n'
+        else:
+            line = ""
+
+        if count == 1:
+            line += "@chapter" + name
+        elif count == 2:
+            line += "@section" + name
+        elif count == 3:
+            line += "@subsection" + name
+        elif count == 4:
+            line += "@subsubsection" + name
+
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+def parse_bulleted_lists(src):
+    lines = src.split('\n')
+    is_list = False
+    for i in range(len(lines)):
+        line = lines[i]
+        if len(line) < 2:
+            continue
+
+        # Detect all of the entries within a list.
+        if is_list:
+            if line.startswith("- "):
+                line = "@item\n" + line[2:]
+            else:
+                line = "@end itemize\n" + line
+                is_list = False
+
+            lines[i] = line
+            continue
+
+        # Detect the start of a list.
+        if line.startswith("- "):
+            line = "@itemize\n@item\n" + line[2:]
+            is_list = True
+            
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+# FIXME: Checking exact indices for specific characters
+#        is quite possibly the worst way to do this.
+def parse_enumerated_lists(src):
+    lines = src.split('\n')
+    is_list = False
+    for i in range(len(lines)):
+        line = lines[i]
+        # Minimum length: "1. a"
+        if len(line) < 4:
+            continue
+
+        # Detect all of the entries within a list.
+        if is_list:
+            if line[0].isdigit() and line[1] == '.' and line[2] == ' ':
+                line = "@item\n" + line[3:]
+            else:
+                line = "@end enumerate\n" + line
+                is_list = False
+
+            lines[i] = line
+            continue
+
+        # Detect the start of a list.
+        if line[0].isdigit() and line[1] == '.' and line[2] == ' ':
+            line = "@enumerate\n@item\n" + line[3:]
+            is_list = True
+            
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+# TODO: Parse nested lists!
+def parse_lists(src):
+    out = src
+    out = parse_bulleted_lists(out)
+    out = parse_enumerated_lists(out)
+    return out
+
+
+def parse_code(src):
+    lines = src.split('\n')
+    is_list = False
+    for i in range(len(lines)):
+        line = lines[i]
+        # Minimum length: "` `"
+        if len(line) < 3:
+            continue
+
+        code_in_quotes = findall(r'`.+?`', line)
+        for str in code_in_quotes:
+            line = line.replace(str, "@code{" + str[1:-1] + "}")
+
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+def parse_images(src):
+    lines = src.split('\n')
+    is_list = False
+    for i in range(len(lines)):
+        line = lines[i]
+        # Minimum length: "![a](b)"
+        if len(line) < 7:
+            continue
+
+        images = findall(r'!\[(.*?)\] *\((.*?)\)', line)
+        for image in images:
+            image_filepath = search(r'(?<=\()(.*?)(?=[\),\", ])', image)
+            line = line.replace(image, "@image{" + image_filepath + "}")
+
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+# NOTE: This function gets confused at images,
+#       so be sure to parse_images() first!
+def parse_links(src):
+    # FIXME: ACTUALLY DO THE THING!
+    return src
+
+    lines = src.split('\n')
+    is_list = False
+    for i in range(len(lines)):
+        line = lines[i]
+        # Minimum length: "[a](b)"
+        if len(line) < 6:
+            continue
+
+        links = findall(r'\[(.+?)\] *\((.+?)\)', line)
+        # FIXME: I need some way to
+        for link in links:
+            line = "@html\n" + line.replace(link, "<a href=\"" + link_path + "\">" + link_name + "</a>") + "\n@end html"
+
+        lines[i] = line
+
+    return '\n'.join(lines)
+
+
+def main():
+    argc = len(argv)
+    if argc < 2:
+        print_usage()
+
+    filepath = ""
+    title = ""
+    output_filepath = ""
+    inline = False
+
+    i = 1
+    while i < argc:
+        arg = argv[i]
+        if "-h" in arg.lower():
+            print_usage()
+        elif arg.startswith("-t"):
+            if i + 1 >= argc:
+                print("ERROR: Expected a title to be specified after `-t` option.")
+                exit(1)
+            i += 1
+            title = argv[i]
+        elif arg.startswith("-o"):
+            if i + 1 >= argc:
+                print("ERROR: Expected an output filepath to be specified after `-o` option.")
+                exit(1)
+            i += 1
+            output_filepath = argv[i]
+        elif arg.startswith("-i") or arg.startswith("--inline"):
+            inline = True
+        else:
+            if not filepath:
+                filepath = arg
+            else:
+                print("ERROR: Multiple filepaths specified (command line argument unrecognized)")
+                exit(1)
+
+        i += 1
+
+    if not filepath:
+        print("ERROR: No filepath was given, or was not able to be parsed from the command line arguments given.")
+        exit(1)
+
+    if not filepath.lower().endswith(".md"):
+        print("ERROR: The path given is not a valid markdown file (wrong extension).")
+        print(" -> ", filepath)
+        exit(1)
+
+    if not title:
+        title = path.basename(filepath)
+
+    if not output_filepath:
+        output_filepath = title.lstrip().lower().replace(' ', '_') + ".tex"
+    
+    with open(filepath) as markdown:
+        with open("template.tex") as template:
+            md = markdown.read()
+            md = parse_headers(md, inline)
+            md = parse_lists(md)
+            md = parse_code(md)
+            md = parse_images(md)
+            md = parse_links(md)
+            tex = template.read()
+            tex = tex.replace("$${{TITLE}}$$", title)
+            tex = tex.replace("$${{CONTENTS}}$$", md)
+            with open(output_filepath, 'w') as out:
+                out.write(tex)
+
+
+if __name__ == "__main__":
+    main()
